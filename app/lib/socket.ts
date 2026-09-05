51350f7f70ba5665b8be023d4426b9d0f8e4eabf
Habibienurseptian <hirokiw12@gmail.com>
2026-09-05 22:48:18 +0700

    path

--- app/lib/socket.ts
+++ app/lib/socket.ts
@@ -4,9 +4,16 @@ import { io, type Socket } from "socket.io-client";
 
 let socket: Socket | null = null;
 
+// export function getSocket() {
+//   if (!socket) {
+//     socket = io({ path: "/api/socket" });
+//   }
+//   return socket;
+// }
+
 export function getSocket() {
   if (!socket) {
-    socket = io({ path: "/api/socket" });
+    socket = io({ path: "https://resume-inky-two-32.vercel.app/" });
   }
   return socket;
 }
